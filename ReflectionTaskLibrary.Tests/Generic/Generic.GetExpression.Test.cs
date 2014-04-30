1c6bd3ecc5b7933c86875dfef4968f6375cdbe51
P. Isaac Hildebrandt <isaac@pihimedia.com>
2014-04-30 16:27:14 -0500

    Resolved work item
    Related Work Items: #1

---
+++ ReflectionTaskLibrary.Tests/Generic/Generic.GetExpression.Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PIHI.ReflectionTaskLibrary;
+
+namespace ReflectionTaskLibrary.Tests.Object
+{
+    [TestClass]
+    public class ObjectGetExpressionTest
+    {
+        private class TestClass
+        {
+            public String TestStringProperty { get; set; }
+            
+            public String TestStringField;
+
+            public String TestMethod()
+            {
+                return "Test method!";
+            }
+        }
+
+        [TestMethod]
+        public void Can_Get_Field_Expression()
+        {
+            var obj = new TestClass();
+
+
+            var exp = GenericGetExpression.GetExpression(obj, "TestStringField");
+            
+            
+            obj.TestStringField = "TestThis";
+
+            var m = exp.Compile();
+            Assert.AreEqual("TestThis", m(obj));
+        }
+
+        [TestMethod]
+        public void Can_Get_Property_Expression()
+        {
+            var obj = new TestClass();
+            var exp = GenericGetExpression.GetExpression(obj, "TestStringProperty");
+            obj.TestStringProperty = "TestThis";
+
+            var m = exp.Compile();
+            Assert.AreEqual("TestThis", m(obj));
+        }
+
+        [TestMethod]
+        public void Can_Get_Method_Expression()
+        {
+            var obj = new TestClass();
+            var exp = GenericGetExpression.GetExpression(obj, "TestMethod");
+            obj.TestStringField = "TestThis";
+
+            var m = exp.Compile();
+            Assert.AreEqual("Test method!", m(obj));
+        }
+    }
+}
---
+++ ReflectionTaskLibrary/Generic/Generic.GetExpression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIHI.ReflectionTaskLibrary
+{
+    public static class GenericGetExpression
+    {
+        public static Expression<Func<T,object>> GetExpression<T>(this T ext, String memberName)
+        {
+            var type = typeof (T);
+            var method = type.GetMethod(memberName, new Type[] {});
+
+            Expression expr;
+
+            if (method != null)
+            {
+                expr = Expression.Call(Expression.Constant(ext), method);
+                
+            }
+            else
+            {
+                expr = Expression.PropertyOrField(Expression.Constant(ext), memberName);
+            }
+
+            var extParam = Expression.Parameter(typeof (T), "ext");
+            return Expression.Lambda<Func<T, object>>(expr, extParam);
+        }
+    }
+}
